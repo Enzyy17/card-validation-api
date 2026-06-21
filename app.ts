f780fc8d4da87d8b46b50fa67da5575bb912526a
Enzyy <enzyygreat@gmail.com>
2026-06-21 20:46:10 +0100

    server update

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-import express, { type Request, type Response } from 'express'
+import express, { response, type Request, type Response } from 'express'
 import dotenv from 'dotenv'
 
 dotenv.config();
 
 const app = express();
-const PORRT = process.env.PORT || 7500
+const PORT = process.env.PORT || 7500
 
+app.get('/', (req, res) => {
+res.json("Welcome to card validation API")
+})
 
 app.use(express.json())
+
+app.listen(PORT, () => {
+console.log(`server running on http://localhost:${PORT}`)
+})
